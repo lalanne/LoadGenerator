bc20e32aea741e185b0b99c3adde6d161f88dd2f
Christian Lalanne <christian.lalanne@zweicom.com>
2017-07-19 00:37:45 -0400

    CHG: formatting request example

--- Requests.cpp
+++ Requests.cpp
@@ -10,7 +10,15 @@ Requests::Requests(const std::string& requests_file) : requests_file(requests_fi
 void Requests::load() {
     cout << "loading requests from file: " << requests_file << endl; 
 
-    const string request = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?><msg><header id_trans=\"1111\" app=\"xml\" user=\"ussd\" passw=\"ussd\" action=\"1\"/><req><op>req_comp_promo</op><msisdn>56999694444</msisdn><idPromo>BO_80MB_2D</idPromo></req></msg>";
+    const string request = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>"
+                            "<msg>"
+                                "<header id_trans=\"1111\" app=\"xml\" user=\"ussd\" passw=\"ussd\" action=\"1\"/>"
+                                "<req>"
+                                    "<op>req_comp_promo</op>"
+                                    "<msisdn>56999694444</msisdn>"
+                                    "<idPromo>BO_80MB_2D</idPromo>"
+                                "</req>"
+                            "</msg>";
 
     requests.push_back(request);
 }
