ed2419622636afdf36c443b81598a98cae8719c9
Christian Lalanne <christian.lalanne@zweicom.com>
2017-07-24 09:09:21 -0400

    ADD: take the message from a file instead of hardwired in the code

--- Requests.cpp
+++ Requests.cpp
@@ -2,25 +2,20 @@
 #include "Requests.hpp"
 
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 Requests::Requests(const std::string& requests_file) : requests_file(requests_file) {}
 
 void Requests::load() {
-    cout << "loading requests from file: " << requests_file << endl; 
+    ifstream fin(requests_file);
+    string content((istreambuf_iterator<char>(fin)), (istreambuf_iterator<char>()));
 
-    const string request = "<?xml version=\"1.0\" encoding=\"ISO-8859-1\"?>"
-                            "<msg>"
-                                "<header id_trans=\"1111\" app=\"xml\" user=\"ussd\" passw=\"ussd\" action=\"1\"/>"
-                                "<req>"
-                                    "<op>req_comp_promo</op>"
-                                    "<msisdn>56999694444</msisdn>"
-                                    "<idPromo>BO_80MB_2D</idPromo>"
-                                "</req>"
-                            "</msg>";
+    content.pop_back();
+    cout << "[" << content << "]" << endl;
 
-    requests.push_back(request);
+    requests.push_back(content);
 }
 
 string Requests::request() {
