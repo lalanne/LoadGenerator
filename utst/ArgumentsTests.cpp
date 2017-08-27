15b1b390cd245d3b06f8a35a3a9b8e2d479d83f7
orc <lalanne@gmail.com>
2017-08-27 00:23:00 -0300

    #5 ADD: tests for are_valid tests

--- utst/ArgumentsTests.cpp
+++ utst/ArgumentsTests.cpp
@@ -3,7 +3,7 @@
 
 #include <gtest/gtest.h>
 
-TEST(ArgumentsTests, constructor) {
+TEST(ArgumentsTests, construction) {
     const int SOME_NUMBER_OF_ARGUMENTS = 3;
 
     Arguments args(SOME_NUMBER_OF_ARGUMENTS, nullptr);
@@ -13,4 +13,20 @@ TEST(ArgumentsTests, constructor) {
 
     EXPECT_EQ(EXPECTED_NUMBER_OF_REQUESTS_IN_PARALLEL, args.NUMBER_OF_REQUESTS_IN_PARALLEL);
     EXPECT_EQ(EXPECTED_NUMBER_OF_TIMES, args.NUMBER_OF_TIMES);
+}
+
+TEST(ArgumentsTests, are_valid) {
+    const int CORRECT_NUMBER_OF_ARGUMENTS = 4;
+
+    Arguments args(CORRECT_NUMBER_OF_ARGUMENTS, nullptr);
+
+    EXPECT_TRUE(args.are_valid());
+}
+
+TEST(ArgumentsTests, are_not_valid) {
+    const int INCORRECT_NUMBER_OF_ARGUMENTS = 3;
+
+    Arguments args(INCORRECT_NUMBER_OF_ARGUMENTS, nullptr);
+
+    EXPECT_FALSE(args.are_valid());
 }
