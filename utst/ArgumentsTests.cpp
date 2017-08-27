d592e587c681c4fa3bb8bfd1f790dba5f6ec777e
orc <lalanne@gmail.com>
2017-08-27 00:46:17 -0300

    #5 ADD: new tests for save method

--- utst/ArgumentsTests.cpp
+++ utst/ArgumentsTests.cpp
@@ -3,6 +3,10 @@
 
 #include <gtest/gtest.h>
 
+#include <string>
+
+using namespace std;
+
 TEST(ArgumentsTests, construction) {
     const int SOME_NUMBER_OF_ARGUMENTS = 3;
 
@@ -30,3 +34,21 @@ TEST(ArgumentsTests, are_not_valid) {
 
     EXPECT_FALSE(args.are_valid());
 }
+
+TEST(ArgumentsTests, save) {
+    const int INCORRECT_NUMBER_OF_ARGUMENTS = 3;
+    const string EXPECTED_REQUESTS_FILE("file.dat");
+    const int EXPECTED_NUMBER_OF_TIMES = 100;
+    const int EXPECTED_NUMBER_OF_REQUESTS_IN_PARALLEL = 10;
+    
+    char* c[] = { "loadGenerator", "10", "100", "file.dat" };
+
+    Arguments args(INCORRECT_NUMBER_OF_ARGUMENTS, c);
+    args.save();
+
+    EXPECT_EQ(EXPECTED_NUMBER_OF_REQUESTS_IN_PARALLEL, args.NUMBER_OF_REQUESTS_IN_PARALLEL);
+    EXPECT_EQ(EXPECTED_NUMBER_OF_TIMES, args.NUMBER_OF_TIMES);
+    EXPECT_STREQ(EXPECTED_REQUESTS_FILE.c_str(), args.REQUESTS_FILE.c_str());
+}
+
+
