0269328312745008a6f7d31c0d2502f964a84f98
orc <lalanne@gmail.com>
2017-09-14 23:33:26 -0300

    #28 ADD: handle if connection cannot be established

--- policies.cpp
+++ policies.cpp
@@ -21,15 +21,21 @@ pair<string, double> request_response(const unsigned int index, Requests& reques
 
     tcp::socket s(io_service);
     tcp::resolver resolver(io_service);
-    boost::asio::connect(s, resolver.resolve({SERVER_HOST, SERVER_PORT}));
+    boost::system::error_code ec;
+    boost::asio::connect(s, resolver.resolve({SERVER_HOST, SERVER_PORT}), ec);
+    if(!ec) {
 
-    boost::asio::write(s, boost::asio::buffer(requests.request(), requests.request().size()));
+        boost::asio::write(s, boost::asio::buffer(requests.request(), requests.request().size()));
 
-    char response[RESPONSE_MAX_LENGTH];
-    boost::asio::read(s, boost::asio::buffer(response, RESPONSE_MAX_LENGTH));
+        char response[RESPONSE_MAX_LENGTH];
+        boost::asio::read(s, boost::asio::buffer(response, RESPONSE_MAX_LENGTH));
 
-    auto end = chrono::steady_clock::now();
-    auto diff = end-start;
+        auto end = chrono::steady_clock::now();
+        auto diff = end-start;
 
-    return make_pair(string(response), chrono::duration <double, milli> (diff).count());
+        return make_pair(string(response), chrono::duration <double, milli> (diff).count());
+    }
+    else {
+        return make_pair("connection fails", 0.0f);
+    }
 }
