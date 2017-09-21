9837d88ad8437af349be8c1f20c493a5be01f8c2
orc <lalanne@gmail.com>
2017-09-21 09:32:02 -0300

    #34 getting there

--- tst/coverage.sh
+++ tst/coverage.sh
@@ -2,10 +2,12 @@
 
 lcov --directory itst/ --capture --output-file coverage_itst.info # capture coverage info
 lcov --directory utst/ --capture --output-file coverage_utst.info # capture coverage info
-lcov --remove coverage_itst.info '/usr/*' --output-file coverage_itst.info # filter out system
-lcov --remove coverage_itst.info 'tst/itst/*' --output-file coverage_itst.info # filter out system
-lcov --remove coverage_utst.info '/usr/*' --output-file coverage_utst.info # filter out system
-lcov --remove coverage_utst.info 'tst/utst/*' --output-file coverage_utst.info # filter out system
 lcov --add-tracefile coverage_itst.info --add-tracefile coverage_utst.info --output-file coverage_total.info #combining tracefiles
+lcov --remove coverage_total.info '/usr/*' --output-file coverage_total.info # filter out system
+path=`pwd`
+utst_path=${path}/utst/*
+itst_path=${path}/itst/*
+lcov --remove coverage_total.info $utst_path --output-file coverage_total.info # filter out system
+lcov --remove coverage_total.info $itst_path --output-file coverage_total.info # filter out system
 lcov --list coverage_total.info #debug info
 
